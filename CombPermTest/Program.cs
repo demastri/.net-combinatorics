ef4a430ef7cceb3c2f283e7ef9e6fc2ecf7c1256
demastri <gear@demastri.com>
2014-06-11 09:27:18 -0500

    remove "Common Tools" (different, unrelated work)

--- CombPermTest/Program.cs
+++ CombPermTest/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Sudoku.CommonTools;
 using JPD.Combinatorics;
 
 namespace CombPermTest
